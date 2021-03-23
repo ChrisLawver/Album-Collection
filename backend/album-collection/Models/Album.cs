355188a69ef497fe8d6fc03c46c57b952ceff7ae
Adam Pryatel <adampryatel@gmail.com>
2021-03-23 15:02:00 -0400

    Added Constructors to Album Model

--- backend/album-collection/Models/Album.cs
+++ backend/album-collection/Models/Album.cs
@@ -9,12 +9,30 @@ namespace album_collection.Models
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string AlbumName { get; set; }
+
+        public string BandName { get; set; }
 
         public string Image { get; set; }
 
+        public string RecordLabel { get; set; }
+
         public IEnumerable<Songs> Songs { get; set; }      // In Production so this will be red until built
 
-        public string RecordLabel { get; set; }
+
+        public Album()
+        {
+        }
+
+        public Album(int id, string albumName, string bandName, string image, string recordLabel)
+        {
+            Id = id;
+            AlbumName = albumName;
+            BandName = bandName;
+            Image = image;
+            RecordLabel = recordLabel;
+        }
     }
+
+       
 }
