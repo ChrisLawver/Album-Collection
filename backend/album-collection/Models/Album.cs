692208635a0bd1373a63e21123eb59931af8793b
Adam Pryatel <adampryatel@gmail.com>
2021-03-23 14:48:18 -0400

    Added Album Model

--- backend/album-collection/Models/Album.cs
+++ backend/album-collection/Models/Album.cs
@@ -7,5 +7,14 @@ namespace album_collection.Models
 {
     public class Album
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+        public IEnumerable<Songs> Songs { get; set; }      // In Production so this will be red until built
+
+        public string RecordLabel { get; set; }
     }
 }
