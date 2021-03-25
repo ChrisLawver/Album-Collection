fddddf44c4d81cd7f9eecb1d4d8491dc5d187440
Chris Lawver <chris_lawver@yahoo.com>
2021-03-25 15:05:20 -0400

    added header and footer js files

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,1 +1,31 @@
-document.getElementById('app').innerText = "Hello World!";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Home from "./components/home";
+
+const appDiv = document.getElementById("app");
+
+export default() => {
+    // document.getElementById('app').innerText = "Hello World!";
+    setupHeader();
+    setupFooter();
+    navHome();
+    
+    appDiv.innerHTML = Home();
+}
+
+function setupHeader(){
+    const headerElement = document.querySelector(".header");
+    headerElement.innerHTML = Header();
+}
+
+function setupFooter(){
+    const footerElement = document.querySelector(".footer");
+    footerElement.innerHTML = Footer();
+}
+
+function navHome(){
+    const homeLink = document.querySelector(".nav_home");
+    homeLink.addEventListener('click', function(){
+        appDiv.innerHTML = Home();
+    });
+}
