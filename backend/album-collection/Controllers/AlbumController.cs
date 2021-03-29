b9f029ce75731f595f3168ffe7f6a0394dac96b5
Adam Pryatel <adampryatel@gmail.com>
2021-03-29 14:36:33 -0400

    Album controller

--- backend/album-collection/Controllers/AlbumController.cs
+++ backend/album-collection/Controllers/AlbumController.cs
@@ -39,66 +39,32 @@ namespace album_collection.Controllers
         //// PUT: api/Albums/5
         //// To protect from overposting attacks, enable the specific properties you want to bind to, for
         //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutAlbum(int id, Album album)
-        //{
-        //    if (id != album.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        [HttpPut("{id}")]
+        public Album PutAlbum(int id, Album album)
+        {
+            if(id != album.Id)
+            {
+                return null;
+            }
 
-        //    _context.Entry(album).State = EntityState.Modified;
+            _albumRepo.Update(album);
+            return album;
+        }
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!AlbumExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+        [HttpPost]
+        public Album PostAlbum([FromBody]Album album)
+        {
+            _albumRepo.Create(album);
+            return album;
+        }
 
-        //    return NoContent();
-        //}
+        [HttpDelete("{id}")]
+        public string DeleteAlbum(int id)
+        {
+            var album = _albumRepo.GetById(id);
+            _albumRepo.Delete(album);
+            return "Album Deleted.";
+        }
 
-        //// POST: api/Albums
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
-        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPost]
-        //public async Task<ActionResult<Album>> PostAlbum(Album album)
-        //{
-        //    _context.Albums.Add(album);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction("GetAlbum", new { id = album.Id }, album);
-        //}
-
-        //// DELETE: api/Albums/5
-        //[HttpDelete("{id}")]
-        //public async Task<ActionResult<Album>> DeleteAlbum(int id)
-        //{
-        //    var album = await _context.Albums.FindAsync(id);
-        //    if (album == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Albums.Remove(album);
-        //    await _context.SaveChangesAsync();
-
-        //    return album;
-        //}
-
-        //private bool AlbumExists(int id)
-        //{
-        //    return _context.Albums.Any(e => e.Id == id);
-        //}
     }
 }
